f86c50b3dae54d4df52af1f2c68ad9ead3936e8a
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-07-18 21:20:26 +0300

    RoutingNotesPage

--- Database.php
+++ Database.php
@@ -3,23 +3,21 @@
 class Database
 {
     public $connection;
-    public function __construct($config,$userName = 'root', $password = 'database@.@912')
+
+    public function __construct($config, $userName = 'root', $password = 'database@.@912')
     {
- 
-        // You can initialize your database connection here if needed
-        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
-        $dsn = 'mysql:'.http_build_query($config, '', ';');
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
         $this->connection = new PDO($dsn, $userName, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
     }
-    // query method to execute SQL queries injections 
-    public function query($query,$params =[])
-    {
 
-        $statment = $this->connection->prepare($query);
-        $statment->execute(params: $params);
-        return $statment;
+    // query method to execute SQL queries
+    public function query($query, $params = [])
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute($params); //ح هنا
+        return $statement;
     }
     
 }
--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php
 require "Core/Functions.php";
-// require "route.php";
+require "route.php";
 /// connect to our mysql database
 require "Database.php";
 $config = require "config.php";
 $db = new Database($config);
-$id = $_GET["id"];
+$id = $_GET["id?"] ?? null;
 // echo $id;
-$query = "SELECT * FROM posts WHERE idPosts = ?";
+$query = "SELECT * FROM notes WHERE userId = ?";
 // echo $query;
-$posts = $db->query($query,params: [$id])->fetch();
-
-echo $posts['title'];
+$result = $db->query($query, [1])->fetchAll(PDO::FETCH_ASSOC);
