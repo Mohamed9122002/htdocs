32c5f4201a331ca9179f9285d3202953c17197a6
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-07-19 12:09:28 +0300

    Form Validations

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -3,10 +3,22 @@ $config = require('config.php');
 $db = new Database($config);
 
 $heading = "Create Note";
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $db->query("INSERT INTO notes (body,userId) VALUES (:body,:userId)",[
-        "body"=> $_POST["body"],
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errors = [];
+
+    if(strlen(($_POST['body']) == 0)) {
+        $errors['body'] = "Body is required";
+        
+    }
+        if(strlen(($_POST['body']) > 1000)) {
+        $errors['body'] = "the body more than 1000 characters";
+        
+    }
+    if(empty($errors)) {
+       $db->query("INSERT INTO notes (body,userId) VALUES (:body,:userId)",[
+        'body'=> $_POST['body'],
         "userId"=> 1,
-    ]);
+    ]);  
+    }
 }
 require "Views/create.view.php";
