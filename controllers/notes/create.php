e2cdf7286a3be8229fb5530f6349ac57e4479ee8
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-07-19 11:36:19 +0300

    Always Escape Untrusted Inpu

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,6 +1,12 @@
 <?php
+$config = require('config.php');
+$db = new Database($config);
+
 $heading = "Create Note";
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    dd($_POST);
+    $db->query("INSERT INTO notes (body,userId) VALUES (:body,:userId)",[
+        "body"=> $_POST["body"],
+        "userId"=> 1,
+    ]);
 }
 require "Views/create.view.php";
