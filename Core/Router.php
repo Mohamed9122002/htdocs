591d45d4b1326d0d3a3d677f300efca7536f7968
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-07-21 09:19:26 +0300

    Middleware

--- Core/Router.php
+++ Core/Router.php
@@ -1,5 +1,9 @@
 <?php
 namespace Core;
+
+use Core\Middleware\Authenticated;
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
 class Router{
   public  $routes = [];
   public function add ($method,$uri,$controller){
@@ -7,26 +11,41 @@ class Router{
             'uri' => $uri,
             'controller' => $controller,
             'method' => $method,
+            'middleware' => null,
         ];
+        return $this;
   }
     public function get($uri,$controller){
-        $this->add('GET', $uri, $controller);
+       return $this->add('GET', $uri, $controller);
     }
     public function post($uri,$controller){
-        $this->add('POST', $uri, $controller);
+       return $this->add('POST', $uri, $controller);
     }
     public function put($uri,$controller){
-        $this->add('PUT', $uri, $controller);
+      return  $this->add('PUT', $uri, $controller);
     }
     public function delete($uri,$controller){
-        $this->add('DELETE', $uri, $controller);
+       return $this->add('DELETE', $uri, $controller);
     }
     public function patch($uri,$controller){
-        $this->add('PATCH', $uri, $controller);
+       return $this->add('PATCH', $uri, $controller);
+    }
+    public function only($key){
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+        return $this;
     }
     public function route($uri ,$method){
         foreach ($this->routes as $route){
             if ($route['uri'] == $uri && $route['method'] == strtoupper($method)) {
+                // apply middleware check
+                // if($route['middleware'] == 'guest'){
+                //     (new Guest)->handle();
+                // }
+                // if($route['middleware'] == 'authenticated'){
+                //     (new Authenticated) ->handle();
+                // }
+                Middleware::resolve($route['middleware']);
+
                 return require base_path($route['controller']);
             }
         }
--- routes.php
+++ routes.php
@@ -11,13 +11,13 @@
 $router->get('/', 'Controllers/index.php');
 $router->get('/about', 'Controllers/about.php');
 $router->get('/contact', 'Controllers/contact.php');
-$router->get('/notes', 'Controllers/notes/index.php');
+$router->get('/notes', 'Controllers/notes/index.php')->only('authenticated');
 $router->get('/note', 'Controllers/notes/show.php');
 $router->delete('/note', 'Controllers/notes/destroy.php');
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes/create', 'controllers/notes/store.php');
 $router->get('/note/edit', 'controllers/notes/edit.php');
 $router->patch('/note', 'controllers/notes/update.php');
-$router->get('/register', 'controllers/registration/create.php');
+$router->get('/register', 'controllers/registration/create.php')->only('guest');
 $router->post('/register', 'controllers/registration/store.php');
 // dd($router->routes);
